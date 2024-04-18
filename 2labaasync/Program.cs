3dbea10c5e7011e9aa07caf8ac5a527e53bf256c
pvnc228 <mist83550@gmail.com>
2024-04-18 14:18:03 +0300

    2 лаба ООП. Асинхронный и неасинх. код

---
+++ 2labaasync/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        DateTime startTime = DateTime.Now;
+
+        await GetServerResponse("https://api.direct.yandex.ru/", "Server 1");
+        await GetServerResponse("https://catfact.ninja/fact", "Server 2");
+        await GetServerResponse("https://api.coindesk.com/v1/bpi/currentprice.json", "Server 3");
+
+        TimeSpan totalTime = DateTime.Now - startTime;
+        Console.WriteLine($"Total time: {totalTime.TotalMilliseconds} ms");
+    }
+
+    static async Task GetServerResponse(string url, string serverName)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode(); 
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Response from {serverName}:");
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error from {serverName}: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ 2labanonasync/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime startTime = DateTime.Now;
+
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                string response = client.DownloadString("https://api.direct.yandex.ru/");
+                Console.WriteLine("Response from Server 1:");
+                Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Error from Server 1: {ex.Message}");
+            }
+        }
+
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                string response = client.DownloadString("https://catfact.ninja/fact");
+                Console.WriteLine("Response from Server 2:");
+                Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Error from Server 2: {ex.Message}");
+            }
+        }
+
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                string response = client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+                Console.WriteLine("Response from Server 3:");
+                Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Error from Server 3: {ex.Message}");
+            }
+        }
+
+        TimeSpan totalTime = DateTime.Now - startTime;
+        Console.WriteLine($"Total time: {totalTime.TotalMilliseconds} ms");
+    }
+}
