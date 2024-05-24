0f7afae6a53aec4c89d224130469bf0807b3da24
pvnc228 <mist83550@gmail.com>
2024-05-24 08:48:16 +0300

    3 Лаб. Работа по ООП

---
+++ LibraryApi/Data/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using LibraryApi.Models;
+
+namespace LibraryApi.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) {
+           
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ LibraryApi/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
